b077770efdee0fffab3b0d37b9200ecc36c5bd9f
Lalitanthwal <87255952+Lalitanthwal@users.noreply.github.com>
2021-07-12 19:14:03 +0530

    new file added

---
+++ value of key in dictionary.py
@@ -0,0 +1,9 @@
+Dictionary1 = { 'A': 'hello', 'B': 'hi', }
+Dictionary2 = { 'B': 'hey' }
+# Dictionary before Updation
+print("Original Dictionary:")
+print(Dictionary1)
+# update the value of key 'B'
+Dictionary1.update(Dictionary2)
+print("Dictionary after updation:")
+print(Dictionary1)
